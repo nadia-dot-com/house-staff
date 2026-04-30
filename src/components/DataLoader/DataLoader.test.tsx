4bf46617d1350e764121d6dda27426db7f778449
Nadiia Lezhanska <nadiia@Nadiias-MacBook-Pro.local>
2026-04-30 18:41:47 +0200

    delete never read value

--- src/components/DataLoader/DataLoader.test.tsx
+++ src/components/DataLoader/DataLoader.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { DataLoader } from "./DataLoader";
-import { ERROR_MESSAGES } from "@/constants/messages";
 import { AppError } from "@/errors";
 
 vi.mock("../LoadingSpinner/LoadingSpinner", () => ({
